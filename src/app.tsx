97166b0f46ff3e2b5cd0ba66dcd1deb936d08d4c
mohmmedalkhatim <136941730+mohmmedalkhatim@users.noreply.github.com>
2026-02-08 07:59:59 +0200

    refactor: clean up README and improve CSS formatting

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,7 @@
 import { Outlet } from "react-router-dom"
 import Header from "./components/Header"
+
+// @ts-ignore
 import "./index.css"
 import Navbar from "./components/Navbar"
 
@@ -7,10 +9,8 @@ function App() {
   return (
     <>
       <Header />
+      <Outlet />
       <Navbar />
-      <div className="content">
-        <Outlet />
-      </div>
     </>
   )
 }
